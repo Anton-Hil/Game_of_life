8974f5c9a17b058a9842e3de5df357c8c7ca21c8
Anton <anton.gill25@gmail.com>
2023-08-14 17:13:11 +0300

    Modified Game_of_life class
    Redesigned attribute assignment in Game_of_life class. Constructor sets default empty world.
    Defined get_new_world(width, height) method, that sets attributes for Game_of_life class.
    Added populate_world() method. Randomly populates current world with cells

--- Game_of_life.py
+++ Game_of_life.py
@@ -12,27 +12,40 @@ class SingletonsMeta(type):
 
 
 class GameOfLife(metaclass=SingletonsMeta):
-    def __init__(self, width=20, height=20):
-        self.width = width
-        self.height = height
+    def __init__(self):
+        self.width = 20
+        self.height = 20
         self.generation_count = 0
         self.world = self.__generate_field()
 
-    @staticmethod
-    def __get_neighbours(world, position, system=None):
+    def __generate_field(self):
+        return [[0 for _ in range(self.width)] for _ in range(self.height)]
+
+    def __get_neighbours(self, position, system=None):
         if system is None:
             system = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
         neighbour_count = 0
         for item in system:
-            if world[(position[0] + item[0]) % len(world)][(position[1] + item[1]) % len(world[0])] == 1:
+            if self.world[(position[0] + item[0]) % self.width][(position[1] + item[1]) % self.height] == 1:
                 neighbour_count += 1
         return neighbour_count
+
+    def populate_world(self):
+        for row in range(self.height):
+            for col in range(self.height):
+                self.world[row][col] = randint(0, 1)
+
+    def get_new_world(self, width=20, height=20):
+        self.width = width
+        self.height = height
+        self.world = self.__generate_field()
+        self.generation_count = 0
 
     def get_new_generation(self):
         new_world = deepcopy(self.world)
         for row in range(self.height):
             for col in range(self.width):
-                neighbours = self.__get_neighbours(self.world, (row, col))
+                neighbours = self.__get_neighbours((row, col))
                 if self.world[row][col] == 1:
                     if neighbours > 3 or neighbours < 2:
                         new_world[row][col] = 0
@@ -42,5 +55,5 @@ class GameOfLife(metaclass=SingletonsMeta):
         self.world = deepcopy(new_world)
         self.generation_count += 1
 
-    def __generate_field(self):
-        return [[randint(0, 1) for _ in range(self.width)] for _ in range(self.height)]
+
+
